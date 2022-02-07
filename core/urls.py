f2498bdbaf46d85555a5c855f30d43c1f93c8901
stiven4950 <omarrocha4950@gmail.com>
2022-02-07 15:22:52 -0500

    Agregado funcionalidad para editar slides de ofertas

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import ItemCarrouselViewSet
+from .views import ItemCarrouselCreate, ItemCarrouselUpdateDelete, ItemCarrouselViewSet
 
 core_patterns = ([
     path('slider/', ItemCarrouselViewSet.as_view()),
+    path('slider-modify/<int:pk>', ItemCarrouselUpdateDelete.as_view()),
+    path('slider-modify/', ItemCarrouselCreate.as_view()),
 ], 'core')
--- core/views.py
+++ core/views.py
@@ -1,37 +1,39 @@
-from rest_framework import generics, mixins
+from rest_framework import generics, mixins, status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from .models import ItemCarrousel
 from .serializers import ItemCarrouselSerializer
 
 class ItemCarrouselUpdateDelete(
-        mixins.CreateModelMixin,
         mixins.RetrieveModelMixin,
-        mixins.ListModelMixin,
         mixins.DestroyModelMixin,
         mixins.UpdateModelMixin,
         generics.GenericAPIView
     ):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = ItemCarrousel.objects.all()
     serializer_class = ItemCarrouselSerializer
     
     def get(self, request, pk):
-        return self.retrieve(request, pk)
-    
-    def post(self, request):
-        return self.create(request)
+        if request.user.is_staff:
+            return self.retrieve(request, pk)
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
     
     def put(self, request, pk):
-        return self.update(request, pk)
+        if request.user.is_staff:
+            return self.update(request, pk)
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
     
     def delete(self, request, pk):
-        return self.destroy(request, pk)
+        if request.user.is_staff:
+            return self.destroy(request, pk)
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
 
 class ItemCarrouselViewSet(
-        mixins.CreateModelMixin,
-        mixins.RetrieveModelMixin,
         mixins.ListModelMixin,
-        mixins.DestroyModelMixin,
-        mixins.UpdateModelMixin,
         generics.GenericAPIView
     ):
     queryset = ItemCarrousel.objects.all()
@@ -39,3 +41,17 @@ class ItemCarrouselViewSet(
 
     def get(self, request):
         return self.list(request)
+
+class ItemCarrouselCreate(
+    generics.GenericAPIView,
+    mixins.CreateModelMixin
+    ):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = ItemCarrousel.objects.all()
+    serializer_class = ItemCarrouselSerializer
+    
+    def post(self, request):
+        if request.user.is_staff:
+            return self.create(request)
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
