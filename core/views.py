031aa719b23774ddd8fcb7c3e2f63695410d51d8
stiven4950 <omarrocha4950@gmail.com>
2022-02-07 16:03:00 -0500

    Optimizacion en acceso para editar Slides

--- core/views.py
+++ core/views.py
@@ -1,7 +1,6 @@
-from rest_framework import generics, mixins, status
+from rest_framework import generics, mixins
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser
 
 from .models import ItemCarrousel
 from .serializers import ItemCarrouselSerializer
@@ -13,24 +12,18 @@ class ItemCarrouselUpdateDelete(
         generics.GenericAPIView
     ):
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAdminUser]
     queryset = ItemCarrousel.objects.all()
     serializer_class = ItemCarrouselSerializer
     
     def get(self, request, pk):
-        if request.user.is_staff:
-            return self.retrieve(request, pk)
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
+        return self.retrieve(request, pk)
     
     def put(self, request, pk):
-        if request.user.is_staff:
-            return self.update(request, pk)
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
+        return self.update(request, pk)
     
     def delete(self, request, pk):
-        if request.user.is_staff:
-            return self.destroy(request, pk)
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
+        return self.destroy(request, pk)
 
 class ItemCarrouselViewSet(
         mixins.ListModelMixin,
@@ -47,11 +40,9 @@ class ItemCarrouselCreate(
     mixins.CreateModelMixin
     ):
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAdminUser]
     queryset = ItemCarrousel.objects.all()
     serializer_class = ItemCarrouselSerializer
     
     def post(self, request):
-        if request.user.is_staff:
-            return self.create(request)
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
+        return self.create(request)
